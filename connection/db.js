7e6e5968b0f9538eaa14d9faf2a8aa6ece1cf8b9
guisantosalves <workguisantos@gmail.com>
2022-05-03 13:31:48 -0400

    testando nova string

--- connection/db.js
+++ connection/db.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    //deve ser uma function para exportar a conexao
-    mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
-        if(!err){
-            console.log("Conectado com sucesso")
-        }else{
-            console.log("não está conectado")
-        }
-    });
+  //deve ser uma function para exportar a conexao
+  mongoose.connect(
+    "mongodb+srv://guizaodozap:paodequeijo123@cluster0.wx08a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err) => {
+      if (!err) {
+        console.log("Conectado com sucesso");
+      } else {
+        console.log("não está conectado");
+      }
+    }
+  );
 };

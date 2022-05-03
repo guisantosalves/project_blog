c13f652f557fa7293ce1ea52d9aeb02ebc37f2c1
guisantosalves <workguisantos@gmail.com>
2022-05-03 14:40:35 -0400

    deu certo o deploy

--- connection/db.js
+++ connection/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-const db = process.env.MONGODB_URL;
+const db =
+  process.env.MONGODB_URL ||
+  "mongodb://guizaodozap:paodequeijo123@cluster0-shard-00-00.wx08a.mongodb.net:27017,cluster0-shard-00-01.wx08a.mongodb.net:27017,cluster0-shard-00-02.wx08a.mongodb.net:27017/blog?ssl=true&replicaSet=atlas-10xsiq-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 module.exports = () => {
-  //deve ser uma function para exportar a conexao
-  mongoose.connect(
-    db,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (!err) {
-        console.log("Conectado com sucesso");
-      } else {
-        console.log("não está conectado");
-      }
-    }
-  );
+
+    mongoose.connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        })
+    .then(()=>{
+        console.log("Database Conectada")
+    })
+    .catch(err=>console.log(err))
+    
 };
